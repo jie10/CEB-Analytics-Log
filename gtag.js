a189db720ca6161be16c12635d04a1dd2816854d
jie10 <benjieburillo10@gmail.com>
2022-09-06 14:14:19 +0800

    Update gtag.js

--- gtag.js
+++ gtag.js
@@ -2,3 +2,17 @@ gtag('event', 'exception', {
   'description': 'error_description',
   'fatal': true,   // set to true if the error is fatal
 })
+
+try {
+
+  alert('Start of try runs')  // (1) <--
+
+  // ...no errors here
+
+  alert('End of try runs')   // (2) <--
+
+} catch (err) {
+
+  alert('Catch is ignored, because there are no errors') // (3)
+
+}
